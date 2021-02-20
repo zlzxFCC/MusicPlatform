47ff5e3ca95062d395f0e465c395ec771dd234a4
Dmitrii Danshin <dmitrii.danshin@timelysoft.net>
2021-02-20 23:13:25 +0600

    Initial Project

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const Index = () => {
+  return (
+    <div>
+      Главная страница
+    </div>
+  );
+};
+
+export default Index;
