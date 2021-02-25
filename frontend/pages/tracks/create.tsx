54e8588b102ff93a136b4ee94b99db4724bdb1d8
Dmitrii Danshin <dmitrii.danshin@timelysoft.net>
2021-02-25 21:39:26 +0600

    Frontend: Add component FileUpload

--- frontend/pages/tracks/create.tsx
+++ frontend/pages/tracks/create.tsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 import MainLayout from '../../layouts/MainLayout'
 import StepWrapper from '../../components/StepWrapper/StepWrapper'
 import { Button, Grid, TextField } from '@material-ui/core'
+import FileUpload from '../../components/FileUpload/FileUpload'
 
 const Create = () => {
   const [activeStep, setActiveStep] = useState(0)
+  const [picture, setPicture] = useState(null)
+  const [audio, setAudio] = useState(null)
 
   const next = () => setActiveStep(prev => prev + 1)
   const prev = () => setActiveStep(prev => prev - 1)
@@ -14,11 +17,21 @@ const Create = () => {
     <MainLayout>
       <StepWrapper activeStep={activeStep}>
         {activeStep === 0 && (
-          <Grid container direction='column' style{{padding : 20}}>
-            <TextField style={{marginTop: 10}} label="Название трека"/>
-            <TextField style={{marginTop: 10}} label="Имя исполнителя"/>
-            <TextField style={{marginTop: 10}} label="Слова к треку" multiline rows={3}/>
+          <Grid container direction='column' style={{ padding: 20 }}>
+            <TextField style={{ marginTop: 10 }} label="Название трека" />
+            <TextField style={{ marginTop: 10 }} label="Имя исполнителя" />
+            <TextField style={{ marginTop: 10 }} label="Слова к треку" multiline rows={3} />
           </Grid>
+        )}
+        {activeStep === 1 && (
+          <FileUpload  setFile={setPicture} accept='image/*'>
+            <Button>Загрузите обложку</Button>
+          </FileUpload>
+        )}
+        {activeStep === 2 && (
+          <FileUpload setFile={setAudio} accept='audio/*'>
+            <Button>Загрузите аудио</Button>
+          </FileUpload>
         )}
       </StepWrapper>
       <Grid justifyContent="space-between">
